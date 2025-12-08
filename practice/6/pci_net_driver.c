77dfcf8320f236684d8666636f8a09ac0756b2b9
andreybedretdinov <andrewbedretdinov0@gmail.com>
2025-12-08 13:00:01 +0300

    pz6

--- practice/6/pci_net_driver.c
+++ practice/6/pci_net_driver.c
@@ -47,10 +47,11 @@ static const struct net_device_ops net_ops = {
 // probe PCI
 static int pci_demo_probe(struct pci_dev *pdev,
                           const struct pci_device_id *id) {
-  int i;
 
   pr_info("pci_net: probe вызван для устройства %04x:%04x\n", id->vendor,
           id->device);
+
+  pci_set_master(pdev);
 
   if (pci_enable_device(pdev))
     return -ENODEV;
@@ -68,20 +69,26 @@ static int pci_demo_probe(struct pci_dev *pdev,
 
   mmio = pci_iomap(pdev, 0, 0);
   if (!mmio) {
-      pr_err("pci_net: не удалось ioremap BAR0\n");
-      free_netdev(ndev);
-      return -ENODEV;
+    pr_err("pci_net: не удалось ioremap BAR0\n");
+    free_netdev(ndev);
+    return -ENODEV;
   }
+
+  ndev->mem_start = (unsigned long)mmio;
 
   ral = readl(mmio + 0x5400);
   rah = readl(mmio + 0x5404);
 
-  ndev->dev_addr[0] = ral & 0xff;
-  ndev->dev_addr[1] = (ral >> 8) & 0xff;
-  ndev->dev_addr[2] = (ral >> 16) & 0xff;
-  ndev->dev_addr[3] = (ral >> 24) & 0xff;
-  ndev->dev_addr[4] = rah & 0xff;
-  ndev->dev_addr[5] = (rah >> 8) & 0xff;
+  unsigned char mac[ETH_ALEN];
+
+  mac[0] = ral & 0xff;
+  mac[1] = (ral >> 8) & 0xff;
+  mac[2] = (ral >> 16) & 0xff;
+  mac[3] = (ral >> 24) & 0xff;
+  mac[4] = rah & 0xff;
+  mac[5] = (rah >> 8) & 0xff;
+
+  eth_hw_addr_set(ndev, mac);
 
   pr_info("pci_net: MAC-адрес = %pM\n", ndev->dev_addr);
 
@@ -105,7 +112,10 @@ static void pci_demo_remove(struct pci_dev *pdev) {
 
   pr_info("pci_net: remove вызван, модуль выгружается\n");
 
+  void __iomem *mmio = (void __iomem *) dev->mem_start;
+
   unregister_netdev(dev);
+  pci_iounmap(pdev, mmio);
   free_netdev(dev);
 
   pci_disable_device(pdev);
