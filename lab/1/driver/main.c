d758dd78ea931494e3f738a80908cd1b9364ee0b
andreybedretdinov <andrewbedretdinov0@gmail.com>
2025-12-14 20:34:04 +0300

    lab1

--- lab/1/driver/main.c
+++ lab/1/driver/main.c
@@ -1,10 +1,10 @@
-#include <linux/module.h>
-#include <linux/fs.h>
-#include <linux/uaccess.h>
 #include <linux/cdev.h>
 #include <linux/device.h>
-#include <linux/jiffies.h>
+#include <linux/fs.h>
 #include <linux/ioctl.h>
+#include <linux/jiffies.h>
+#include <linux/module.h>
+#include <linux/uaccess.h>
 
 #define DEVICE_NAME "lab1dev"
 #define MAX_BINS 20
@@ -23,20 +23,18 @@ static unsigned long last_write_time = 0;
 static size_t histogram[MAX_BINS] = {0};
 static int hist_len = MAX_BINS;
 
-static int dev_open(struct inode *i, struct file *f)
-{
+static int dev_open(struct inode *i, struct file *f) {
   pr_info("lab1: open\n");
   return 0;
 }
 
-static int dev_close(struct inode *i, struct file *f)
-{
+static int dev_close(struct inode *i, struct file *f) {
   pr_info("lab1: close\n");
   return 0;
 }
 
-static ssize_t dev_write(struct file *f, const char __user *buf, size_t len, loff_t *off)
-{
+static ssize_t dev_write(struct file *f, const char __user *buf, size_t len,
+                         loff_t *off) {
   if (len < sizeof(int))
     return -EINVAL;
 
@@ -47,8 +45,8 @@ static ssize_t dev_write(struct file *f, const char __user *buf, size_t len, lof
   return sizeof(int);
 }
 
-static ssize_t dev_read(struct file *f, char __user *buf, size_t len, loff_t *off)
-{
+static ssize_t dev_read(struct file *f, char __user *buf, size_t len,
+                        loff_t *off) {
   unsigned long dt = jiffies - last_write_time;
   unsigned long dt_us = jiffies_to_usecs(dt);
   int bin = dt_us / BIN_WIDTH_US;
@@ -64,8 +62,7 @@ static ssize_t dev_read(struct file *f, char __user *buf, size_t len, loff_t *of
   return sizeof(int);
 }
 
-static long dev_ioctl(struct file *f, unsigned int cmd, unsigned long arg)
-{
+static long dev_ioctl(struct file *f, unsigned int cmd, unsigned long arg) {
   switch (cmd) {
   case IOCTL_GET_HIST_LEN:
     if (copy_to_user((int *)arg, &hist_len, sizeof(hist_len)))
@@ -84,29 +81,32 @@ static long dev_ioctl(struct file *f, unsigned int cmd, unsigned long arg)
   return 0;
 }
 
-static struct file_operations fops = {
-    .owner = THIS_MODULE,
-    .open = dev_open,
-    .release = dev_close,
-    .write = dev_write,
-    .read = dev_read,
-    .unlocked_ioctl = dev_ioctl
-};
+static struct file_operations fops = {.owner = THIS_MODULE,
+                                      .open = dev_open,
+                                      .release = dev_close,
+                                      .write = dev_write,
+                                      .read = dev_read,
+                                      .unlocked_ioctl = dev_ioctl};
 
-static int __init lab1_init(void)
-{
+static int __init lab1_init(void) {
   alloc_chrdev_region(&dev_num, 0, 1, DEVICE_NAME);
+
   cdev_init(&c_dev, &fops);
   cdev_add(&c_dev, dev_num, 1);
+
   cl = class_create(DEVICE_NAME);
+  if (IS_ERR(cl)) {
+    unregister_chrdev_region(dev_num, 1);
+    return PTR_ERR(cl);
+  }
+
   device_create(cl, NULL, dev_num, NULL, DEVICE_NAME);
 
   pr_info("lab1: driver loaded\n");
   return 0;
 }
 
-static void __exit lab1_exit(void)
-{
+static void __exit lab1_exit(void) {
   device_destroy(cl, dev_num);
   class_destroy(cl);
   cdev_del(&c_dev);
